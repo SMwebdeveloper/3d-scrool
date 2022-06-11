860264e97a16ccac8608415a8d2a069a5e20f53f
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2022-06-11 20:25:53 +0500

    3d scrool

---
+++ js/app.js
@@ -0,0 +1,46 @@
+let zSpacing = -1000,
+    lastPost = zSpacing / 5,
+    $frames = document.getElementsByClassName('frame'),
+    frames = Array.from($frames),
+    zVals = []
+
+window.onscroll = function () {
+    let top = document.documentElement.scrollTop,
+        delta = lastPost -top
+    
+    lastPost = top
+    frames.forEach((n, i) => {
+        zVals.push((i * zSpacing) + zSpacing)
+        zVals[i]  += delta * -5.5
+
+        let frame = frames[i],
+            transform = `translateZ(${zVals[i]}px)`,
+            opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+        frame.setAttribute('style' , `transform: ${transform}; opacity:${opacity}`)
+    });
+}
+
+window.scrollTo(0, 1)
+
+
+
+
+
+// audio
+
+let soundbutton = document.querySelector('.soundbutton')
+    audio = document.querySelector('.audio')
+
+
+soundbutton.addEventListener('click', (e) => {
+    soundbutton.classList.toggle('paused')
+    audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function(){
+    soundbutton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+    audio.pause()
+}
